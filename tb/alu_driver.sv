06c45fb8448670ba6320f3ac15bbc58fbe4995ae
Bandaru Thirumala sai <thirumalasai.bandaru2007@gmail.com>
2026-09-20 20:07:20 +0530

    Refactor ALU driver formatting
    Reformatted code for better readability and consistency.

--- tb/alu_driver.sv
+++ tb/alu_driver.sv
@@ -3,19 +3,20 @@ class driver;
   virtual riscv_alu_if vif;
 
   function new(virtual riscv_alu_if vif, mailbox gen2drv);
-    this.vif = vif;
+    this.vif     = vif;
     this.gen2drv = gen2drv;
   endfunction
 
   task main();
+    @(posedge vif.rst_n);
     forever begin
       transaction trans;
       gen2drv.get(trans);
-      
-      @(posedge vif.clk); 
-      vif.a <= trans.a;
-      vif.b <= trans.b;
+      @(posedge vif.clk);
+      vif.a           <= trans.a;
+      vif.b           <= trans.b;
       vif.alu_control <= trans.alu_control;
     end
   endtask
+
 endclass
