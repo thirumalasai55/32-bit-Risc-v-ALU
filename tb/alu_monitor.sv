967d58823f8bfcda478d431edc368456fc90660e
Bandaru Thirumala sai <thirumalasai.bandaru2007@gmail.com>
2026-09-20 20:07:52 +0530

    Improve formatting and readability in alu_monitor.sv
    Refactor code for better readability and formatting.

--- tb/alu_monitor.sv
+++ tb/alu_monitor.sv
@@ -3,23 +3,24 @@ class monitor;
   virtual riscv_alu_if vif;
 
   function new(virtual riscv_alu_if vif, mailbox mon2scb);
-    this.vif = vif;
+    this.vif     = vif;
     this.mon2scb = mon2scb;
   endfunction
 
-  task main();
+  task main(); 
+    @(posedge vif.rst_n);
     forever begin
       transaction trans = new();
       @(posedge vif.clk);
-      #1; // Slight delay to ensure the sample stable values after the clock edge
-      
-      trans.a = vif.a;
-      trans.b = vif.b;
+      #1; 
+      trans.a           = vif.a;
+      trans.b           = vif.b;
       trans.alu_control = vif.alu_control;
-      trans.result = vif.result;
-      trans.zero = vif.zero;
-      
+      trans.result      = vif.result;
+      trans.zero        = vif.zero;
+
       mon2scb.put(trans);
     end
   endtask
+
 endclass
